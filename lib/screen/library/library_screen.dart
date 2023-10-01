b6a2585ab3689e106dd9ab806d5170b2b6211c84
mytran324 <92320923+mytran324@users.noreply.github.com>
2023-10-01 15:56:44 +0700

    layout library

--- lib/screen/library/library_screen.dart
+++ lib/screen/library/library_screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_book_app/screen/screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../widget/widget.dart';
@@ -17,6 +18,87 @@ class LibraryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: CustomAppBar(title: 'My library'),
+      body: CustomTab(),
+    );
+  }
+}
+
+// Tab bar
+class CustomTab extends StatefulWidget {
+  const CustomTab({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CustomTabState();
+}
+
+class _CustomTabState extends State<CustomTab> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: null,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                child: TabBar(
+                  indicator: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  labelColor: Colors.white,
+                  unselectedLabelColor: Colors.black,
+                  labelStyle: const TextStyle(fontWeight: FontWeight.w500),
+                  tabs: const [
+                    Tab(
+                      text: 'Collection',
+                    ),
+                    Tab(
+                      text: 'Favourites',
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(children: [
+                  CollectionTab(),
+                  FavouritesTab(),
+                ]),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CollectionTab extends StatelessWidget {
+  const CollectionTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('hello')),
+    );
+  }
+}
+
+class FavouritesTab extends StatelessWidget {
+  const FavouritesTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('hello')),
     );
   }
 }
