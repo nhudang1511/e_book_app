f5b3685216b05e073ddbe8255b427bfad7c10f11
nhu_nguyen <nhudang1511@gmail.com>
2023-10-04 10:27:37 +0700

    update category tag in book_card

--- lib/widget/book_items/book_card_main.dart
+++ lib/widget/book_items/book_card_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../blocs/blocs.dart';
 import '../../model/models.dart';
 
 class BookCardMain extends StatelessWidget {
@@ -14,63 +16,84 @@ class BookCardMain extends StatelessWidget {
       width: MediaQuery.of(context).size.width - 10,
       height: 150,
       margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-      child: Row(
-        children: [
-          Expanded(
-              flex: 1,
-              child: Image.network(book.imageUrl)),
-          SizedBox(width: 5),
-          Expanded(flex:2, child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      child: BlocBuilder<CategoryBloc, CategoryState>(builder: (context, state) {
+        if(state is CategoryLoading){
+          return Center(child: CircularProgressIndicator());
+        }
+        if(state is CategoryLoaded){
+          // Tạo danh sách tên danh mục từ categoryId trong book
+          List<String> categoryNames = [];
+          for (String categoryId in book.categoryId) {
+            Category? category = state.categories.firstWhere(
+                  (cat) => cat.id == categoryId,
+            );
+            if (category != null) {
+              categoryNames.add(category.name);
+            }
+          }
+          return Row(
             children: [
               Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  flex: 1,
+                  child: Image.network(book.imageUrl)),
+              SizedBox(width: 5),
+              Expanded(flex:2, child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(book.title, style: Theme.of(context).textTheme.headlineSmall,),
-                        Text(book.authodId, style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: Color(0xFFC7C7C7), fontWeight: FontWeight.normal),),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(book.title, style: Theme.of(context).textTheme.headlineSmall,),
+                            Text(book.authodId, style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: Color(0xFFC7C7C7), fontWeight: FontWeight.normal),),
+                          ],
+                        ),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.bookmark_outlined, color: Color(0xFFDFE2E0),))
                       ],
                     ),
-                    IconButton(onPressed: (){}, icon: Icon(Icons.bookmark_outlined, color: Color(0xFFDFE2E0),))
-                  ],
-                ),
-              ),
-              SizedBox(height: 35,),
-              Icon(Icons.money_off),
-              Row(children: [
-                Icon(Icons.menu_book),
-                SizedBox(width: 5,),
-                Text(book.language)
-              ],),
-              SizedBox(
-                height: 25,
-                child: ListView.builder(
-                  itemCount: book.categoryId.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                          color: Color(0xFFEB6097),
-                          borderRadius: BorderRadius.circular(5)),
-                      padding: EdgeInsets.all(5),
-                      margin: EdgeInsets.only(top: 2, right: 5),
-                      child: Text(book.categoryId[index],
-                        style: Theme
-                            .of(context)
-                            .textTheme
-                            .bodyLarge!
-                            .copyWith(color: Colors.white),
-                      ),
-                    );
-                  }),
-              )
+                  ),
+                  SizedBox(height: 35,),
+                  Icon(Icons.money_off),
+                  Row(children: [
+                    Icon(Icons.menu_book),
+                    SizedBox(width: 5,),
+                    Text(book.language)
+                  ],),
+                  SizedBox(
+                    height: 25,
+                    child: ListView.builder(
+                        itemCount: categoryNames.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                                color: Color(0xFFEB6097),
+                                borderRadius: BorderRadius.circular(5)),
+                            padding: EdgeInsets.all(5),
+                            margin: EdgeInsets.only(top: 2, right: 5),
+                            child: Text(categoryNames[index],
+                              style: Theme
+                                  .of(context)
+                                  .textTheme
+                                  .bodyLarge!
+                                  .copyWith(color: Colors.white),
+                            ),
+                          );
+                        }),
+                  )
+                ],
+              ))
             ],
-          ))
-        ],
-      ),
+          );
+        }
+        else{
+          return Text("Something went wrong");
+        }
+  },
+),
     );
   }
 }
