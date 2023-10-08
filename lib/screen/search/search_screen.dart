b787edd9d3282ba7af1442d04520f774e25cb15d
nhu_nguyen <nhudang1511@gmail.com>
2023-10-08 21:10:22 +0700

    finish search

--- lib/screen/search/search_screen.dart
+++ lib/screen/search/search_screen.dart
@@ -1,10 +1,10 @@
 import 'package:e_book_app/model/models.dart';
-import 'package:e_book_app/widget/category_items/category_card.dart';
+import 'package:e_book_app/widget/book_items/list_book_main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../blocs/blocs.dart';
 import '../../widget/widget.dart';
+import 'components/list_category_in_search.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -22,82 +22,115 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  String _searchText = "";
+  String _selectedSearchOption = "Name of book"; // Default search option
+  bool _hasSearched = false;
+  void _onSearchTextChanged(String newText) {
+    setState(() {
+      _searchText = newText;
+      _hasSearched = false;
+    });
+  }
+  List<Book> _performSearch(List<Book> books, List<Author> authors) {
+    List<Book> results = [];
+    if (_searchText.isEmpty) {
+      return results;
+    }
+    if (_selectedSearchOption == "Name of book") {
+      results = books
+          .where((book) => book.title.toLowerCase().contains(_searchText.toLowerCase()))
+          .toList();
+    }
+    else if (_selectedSearchOption == "Author") {
+      results = books
+          .where((book) {
+        // Tìm tác giả dựa trên fullName
+        Author? author = authors.firstWhere(
+                (author) => author.id.toLowerCase() == book.authodId.toLowerCase());
+        return author.fullName.toLowerCase().contains(_searchText.toLowerCase());
+      })
+          .toList();
+    }
+    else if (_selectedSearchOption == "Tags") {
+      results = books
+          .where((book) => book.categoryId.contains(_searchText.toLowerCase()))
+          .toList();
+    }
+    _hasSearched = true;
+    return results;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'Search'),
-      //bottomNavigationBar: CustomNavBar(initialIndex: 1,),
       body: Column(
-        children: [
-          SearchAnchor(
-            builder: (BuildContext context, SearchController controller) {
-              return Padding(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Color(0xFFDFE2E0)),
+              child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Color(0xFFDFE2E0)
-                    //border: Border.all(color: Colors.grey), // Đổi màu viền nếu cần
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 5),
-                  child: Row(
-                    children: [
-                      Icon(Icons.search),
-                      SizedBox(width: 8.0),
-                      Expanded(
-                        child: TextField(
-                          controller: controller,
-                          onTap: () {
-                            controller.openView();
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Search for book',
-                            hintStyle: Theme.of(context).textTheme.titleLarge!.copyWith(color: Color(0xFFC9C9C9))
-                          ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: TextField(
+                        onChanged: _onSearchTextChanged,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search for book',
+                          hintStyle: Theme.of(context).textTheme.titleLarge!.copyWith(color: Color(0xFFC9C9C9)),
                         ),
                       ),
-                      Icon(Icons.mic),
-                    ],
-                  ),
+                    ),
+                    Icon(Icons.mic)
+                  ],
                 ),
-              );
-            },
-            suggestionsBuilder: (BuildContext context, SearchController controller) {
-              return List<ListTile>.generate(5, (int index) {
-                final String item = 'item $index';
-                return ListTile(
-                  title: Text(item),
-                  onTap: () {
-                    setState(() {
-                      controller.closeView(item);
-                    });
-                  },
-                );
-              });
-            },
+              ),
+            ),
           ),
-          SizedBox(height: 10),
-          SectionTitle(title: 'Genres: '),
-          BlocBuilder<CategoryBloc, CategoryState>(
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                _buildSearchOptionButton("Name of book"),
+                _buildSearchOptionButton("Author"),
+                _buildSearchOptionButton("Tags"),
+              ],
+            ),
+          ),
+          BlocBuilder<BookBloc, BookState>(
             builder: (context, state) {
-              if(state is CategoryLoading){
-                return Center(child: CircularProgressIndicator());
+              if (state is BookLoading){
+                return CircularProgressIndicator();
               }
-              if(state is CategoryLoaded){
-                return Expanded(
-                  child: GridView.count(
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                    // Create a grid with 2 columns. If you change the scrollDirection to
-                    // horizontal, this produces 2 rows.
-                    crossAxisCount: 2,
-                    // Generate 100 widgets that display their index in the List.
-                    children: List.generate(state.categories.length, (index) {
-                      return CategoryCard(category: state.categories[index]);
-                    }),
-                  ),
+              if(state is BookLoaded){
+                List<Book> book = state.books;
+                return BlocBuilder<AuthorBloc, AuthorState>(
+                  builder: (context, state) {
+                   if(state is AuthorLoaded){
+                     List<Book> searchResults = _performSearch(book, state.authors);
+                     return Expanded(
+                       child: _hasSearched == true ? SizedBox(
+                         height: 180,
+                         child: ListView.builder(
+                             scrollDirection: Axis.vertical,
+                             itemCount: searchResults.length,
+                             itemBuilder: (context,index){
+                               return BookCardMain(book: searchResults[index],);
+                             }),
+                       ) :  ListCategoryInSearch(),
+                     );
+                   }
+                   else{
+                     return Text('Something went wrong');
+                   }
+                    },
                 );
               }
               else{
@@ -105,7 +138,27 @@ class _SearchScreenState extends State<SearchScreen> {
               }},
           ),
         ],
-      )
+      ),
+    );
+  }
+
+  Widget _buildSearchOptionButton(String option) {
+    return Container(
+      margin: EdgeInsets.only(right: 10),
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            _selectedSearchOption = option;
+          });
+        },
+        style: ElevatedButton.styleFrom(
+          primary: _selectedSearchOption == option ? Colors.white : Colors.grey,
+          side: _selectedSearchOption == option ? BorderSide(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary) : null),
+        child: Text(option, style: TextStyle(color: Colors.black),),
+      ),
     );
   }
 }
+
