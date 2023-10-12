943d02b1cf5acfc26d6df217998e7d41530fc542
nhu_nguyen <nhudang1511@gmail.com>
2023-10-12 19:35:23 +0700

    add button navigator in pdf viewer

--- lib/screen/book/book_screen.dart
+++ lib/screen/book/book_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../../model/models.dart';
@@ -21,6 +23,8 @@ class _BookScreenState extends State<BookScreen> {
   late PdfViewerController _pdfViewerController;
   OverlayEntry? _overlayEntry;
   final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+  int currentPage = 1;
+
   @override
   void initState() {
     _pdfViewerController = PdfViewerController();
@@ -28,47 +32,87 @@ class _BookScreenState extends State<BookScreen> {
   }
 
   void _showContextMenu(
-      BuildContext context, PdfTextSelectionChangedDetails details) {
-    final OverlayState _overlayState = Overlay.of(context)!;
+      BuildContext context,
+      PdfTextSelectionChangedDetails details,
+      ) {
+    final OverlayState overlayState = Overlay.of(context)!; // Remove the double underscores
     _overlayEntry = OverlayEntry(
       builder: (context) => Positioned(
         top: details.globalSelectedRegion!.center.dy - 55,
         left: details.globalSelectedRegion!.bottomLeft.dx,
-        child:ElevatedButton(
+        child: ElevatedButton(
           onPressed: () {
             final selectedText = details.selectedText ?? '';
             Clipboard.setData(ClipboardData(text: selectedText));
-            print(
-                'Text copied to clipboard: ' + details.selectedText.toString());
+            print('Text copied to clipboard: ' + details.selectedText.toString());
             _pdfViewerController.clearSelection();
           },
           child: Text('Copy', style: TextStyle(fontSize: 17)),
         ),
       ),
     );
-    _overlayState.insert(_overlayEntry!);
+    overlayState.insert(_overlayEntry!); // Remove the double underscores
+  }
+
+  void _updateCurrentPageNumber(PdfPageChangedDetails details) {
+    setState(() {
+      currentPage = details.newPageNumber;
+    });
+  }
+
+  void nextPage() {
+    _pdfViewerController.nextPage();
+  }
+
+  void previousPage() {
+    _pdfViewerController.previousPage();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-          elevation: 0,
-          iconTheme: const IconThemeData(color: Color(0xFFDFE2E0)),
-          actionsIconTheme: IconThemeData(color: Colors.red),
-          actions: [
-            IconButton(onPressed: (){
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Color(0xFFDFE2E0)),
+        actions: [
+          IconButton(
+            onPressed: () {
               _pdfViewerKey.currentState?.openBookmarkView();
-            }, icon: const Icon(Icons.book, color: Color(0xFFDFE2E0),)),
-            IconButton(onPressed: (){}, icon: const Icon(Icons.settings, color: Color(0xFFDFE2E0),)),
-          ],
+            },
+            icon: const Icon(Icons.book),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          ),
+        ],
       ),
-      body:Directionality(
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              onPressed: previousPage,
+              icon: Icon(Icons.arrow_back_ios_new),
+            ),
+            Text(
+              'Page $currentPage of  ${_pdfViewerController.pageCount}',
+              style: TextStyle(fontSize: 18),
+            ),
+            IconButton(
+              onPressed: nextPage,
+              icon: Icon(Icons.arrow_forward_ios),
+            ),
+          ],
+        ),
+      ),
+      body: Directionality(
         textDirection: TextDirection.ltr,
         child: SfPdfViewer.network(
           'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
           key: _pdfViewerKey,
+          onPageChanged: _updateCurrentPageNumber,
           onTextSelectionChanged: (PdfTextSelectionChangedDetails details) {
             if (details.selectedText == null && _overlayEntry != null) {
               _overlayEntry!.remove();
@@ -80,8 +124,16 @@ class _BookScreenState extends State<BookScreen> {
           controller: _pdfViewerController,
           scrollDirection: PdfScrollDirection.horizontal,
         ),
-      ));
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
 
 
