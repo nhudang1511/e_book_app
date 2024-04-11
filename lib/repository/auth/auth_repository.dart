d4c22b93731ecba3c393a600a346fbab95adbb19
Nguyễn ThịQuỳnh Như <nhudang1511@gmail.com>
2024-04-11 11:15:38 +0700

    fix

--- lib/repository/auth/auth_repository.dart
+++ lib/repository/auth/auth_repository.dart
@@ -21,9 +21,9 @@ class AuthRepository extends BaseAuthRepository {
       final credential = await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
       final user = credential.user;
-      // if(user != null){
-      //   SharedService.setUserId(user.uid);
-      // }
+      if(user != null){
+        SharedService.setUserId(user.uid);
+      }
       return user;
     } catch (_) {}
     return null;
@@ -86,16 +86,16 @@ class AuthRepository extends BaseAuthRepository {
 
       // Obtain the auth details from the request
       final GoogleSignInAuthentication? googleAuth =
-          await googleUser?.authentication;
+      await googleUser?.authentication;
       // Create a new credential
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
       final userCredential =
-          await _firebaseAuth.signInWithCredential(credential);
+      await _firebaseAuth.signInWithCredential(credential);
       if(userCredential.user != null){
-        //SharedService.setUserId(userCredential.user!.uid);
+        SharedService.setUserId(userCredential.user!.uid);
       }
       return userCredential.user;
     } catch (e) {
@@ -111,13 +111,13 @@ class AuthRepository extends BaseAuthRepository {
 
       // Create a credential from the access token
       final OAuthCredential facebookAuthCredential =
-          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+      FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
       // Once signed in, return the UserCredential
       final credential =
-          await _firebaseAuth.signInWithCredential(facebookAuthCredential);
+      await _firebaseAuth.signInWithCredential(facebookAuthCredential);
       if(credential.user != null){
-        //SharedService.setUserId(credential.user!.uid);
+        SharedService.setUserId(credential.user!.uid);
       }
       return credential.user;
     } catch (e) {
